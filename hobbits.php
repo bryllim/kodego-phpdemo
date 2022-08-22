893fc63c1da28af9fd15ce9af6f349dda8397065
bryllim <bryllim@gmail.com>
2022-08-22 20:38:29 +0800

    added simple hobbit api

---
+++ hobbits.php
@@ -0,0 +1,11 @@
+<?php
+
+    $hobbits = array(
+        "Bilbo Baggins" => "The Protagonist",
+        "Bungo Baggins" => "Bilbo's Father",
+        "Belladona Took" => "Bilbo's Mother",
+    );
+
+    echo json_encode($hobbits);
+
+?>
