eff1c9bfe9a0d0439ad6b1dc92c38db04ec7dd20
bryllim <bryllim@gmail.com>
2022-08-25 20:50:10 +0800

    Added exercise for inheritance

--- example.php
+++ example.php
@@ -1,79 +1,40 @@
 <?php
 
-class Pet{
+class Car{
 
     public $name;
-    private $breed;
-    private $color;
-    private $size;
 
-    private $isNeutered=false;
-
-    public function eat(){
-        echo "{$name} is eating.";
+    function __construct($name){
+        $this->name = $name;
     }
 
-    // Getters & setters
-    public function set_breed($value){
-        $this->breed = $value;
-    }
-    public function get_breed(){
-        return $this->breed;
-    }
-    public function set_color($value){
-        $this->color = $value;
-    }
-    public function get_color(){
-        return $this->color;
-    }
-    public function set_size($value){
-        $this->size = $value;
-    }
-    public function get_size(){
-        return $this->size;
-    }
-    public function toggle_isNeutered(){
-        $this->isNeutered = !$this->isNeutered;
-    }
-    public function get_isNeutered(){
-        return $this->isNeutered;
+    function printDetails(){
+        echo "Car Name: {$this->name}";
     }
 
-    public function displayDetails(){
-        return "This is ". ((preg_match('/^[aeiou]/i', $this->breed[0])) ? "an" : "a") ." {$this->breed} and his/her name is {$this->name} and its color is {$this->color} and it is {$this->size} and it is neutered: {$this->isNeutered}";
+    final function honk(){
+        echo "Beep!";
     }
-
 }
 
-class Dog extends Pet{
+class Toyota extends Car{
 
-    public function bark(){
-        echo "Aw aw!";
+    public $country;
+
+    function __construct($name, $country){
+        $this->name = $name;
+        $this->country = $country;
     }
 
-    public function fetch(){
-        echo "Fetching sir! Awoo";
+    function printDetails(){
+        echo "Car Name: {$this->name} - Country: {$this->country}";
     }
-
 }
 
-class Cat extends Pet{
+$toyota = new Toyota("Toyota", "Japan");
+$ford = new Toyota("Ford", "USA");
 
-    public function meow(){
-        echo "Ming ming!";
-    }
-
-}
-
-$dog = new Dog();
-$dog->name = "Bantay";
-$dog->set_breed("Aspin");
-$dog->set_color("Brown");
-$dog->set_size("XXL");
-$dog->toggle_isNeutered();
-echo $dog->displayDetails();
-echo "<hr>";
-$dog->fetch();
-
-
+$toyota->printDetails();
+$ford->printDetails();
+$toyota->honk();
 ?>
